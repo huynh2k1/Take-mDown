2d633b6d9919d9051ea7fdc2966f558d8e165e52
Huynh <huynhnv@solarapp.asia>
2025-12-25 17:53:56 +0700

    Transition Level Selector

--- Assets/_GAME/Scripts/UI/UISelectLevel.cs
+++ Assets/_GAME/Scripts/UI/UISelectLevel.cs
@@ -1,36 +1,100 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class UISelectLevel : BasePopup
 {
     public override UIType Type => UIType.SELECTLEVEL;
 
+    [Header("Pages")]
+    [SerializeField] RectTransform[] _pages;
+    [SerializeField] float _pageWidth = 2500f;
+    [SerializeField] float _tweenTime = 0.35f;
+
+    [Header("Buttons")]
+    [SerializeField] Button _btnPrev;
+    [SerializeField] Button _btnNext;
+
     private LevelButton[] _levelButtons;
+    private int _currentPage = 0;
+    private bool _isTweening;
 
     public static Action<int> OnSelectLevelAction;
 
     protected override void Awake()
     {
         base.Awake();
-        _levelButtons = transform.GetComponentsInChildren<LevelButton>(true);
 
-        foreach(var b in _levelButtons)
-        {
+        _levelButtons = transform.GetComponentsInChildren<LevelButton>(true);
+        foreach (var b in _levelButtons)
             b.OnClickThis += OnSelectLevel;
-        }
+
+        _btnPrev.onClick.AddListener(PrevPage);
+        _btnNext.onClick.AddListener(NextPage);
     }
 
     private void Start()
     {
-        for(int i = 0; i < _levelButtons.Length; i++)
-        {
+        for (int i = 0; i < _levelButtons.Length; i++)
             _levelButtons[i].Init(i);
-        }
+
+        UpdateButtonState();
     }
+
+    #region Page Logic
+
+    void NextPage()
+    {
+        if (_isTweening || _currentPage >= _pages.Length - 1) return;
+        MoveToPage(_currentPage + 1);
+    }
+
+    void PrevPage()
+    {
+        if (_isTweening || _currentPage <= 0) return;
+        MoveToPage(_currentPage - 1);
+    }
+
+    void MoveToPage(int targetPage)
+    {
+        _isTweening = true;
+        _currentPage = targetPage;
+
+        for (int i = 0; i < _pages.Length; i++)
+        {
+            float targetX = (i - _currentPage) * _pageWidth;
+
+            _pages[i]
+                .DOAnchorPosX(targetX, _tweenTime)
+                .SetEase(Ease.OutCubic)
+                .OnComplete(() => _isTweening = false);
+        }
+
+        UpdateButtonState();
+    }
+
+    void UpdateButtonState()
+    {
+        _btnPrev.interactable = _currentPage > 0;
+        _btnNext.interactable = _currentPage < _pages.Length - 1;
+    }
+
+    #endregion
+
+    #region Level Select
 
     void OnSelectLevel(int id)
     {
         Hide();
         OnSelectLevelAction?.Invoke(id);
     }
+
+    private void OnDestroy()
+    {
+        foreach (var b in _levelButtons)
+            b.OnClickThis -= OnSelectLevel;
+    }
+
+    #endregion
 }
